269f846172888c9b923a2dc67d36f76dc980c344
Pedro Ardigó <Pedro Ardig@DESKTOP-JI7ECM2>
2020-08-11 17:45:14 -0300

    FluentValidator no Domain

--- BaltaStore.Domain/StoreContext/Entities/OrderItem.cs
+++ BaltaStore.Domain/StoreContext/Entities/OrderItem.cs
@@ -1,8 +1,9 @@
+using FluentValidator;
 using System.Collections.Generic;
 
 namespace BaltaStore.Domain.StoreContext.Entities
 {
-    public class OrderItem
+    public class OrderItem : Notifiable
     {
 
         public OrderItem(Product product, decimal quantity)
@@ -13,7 +14,7 @@ namespace BaltaStore.Domain.StoreContext.Entities
 
             if (Product.QuantityOnHand < quantity)
             {
-                Notifications.Add("Quantidade", "Produto fora de estoque");
+                AddNotification("Quantidade", "Produto fora de estoque");
             }
 
         }
@@ -22,7 +23,7 @@ namespace BaltaStore.Domain.StoreContext.Entities
         public Product Product { get; private set; }
         public decimal Quantity { get; private set; }
         public decimal Price { get; private set; }
-        public IDictionary<string, string> Notifications {get; set; }
+        
 
 
 
